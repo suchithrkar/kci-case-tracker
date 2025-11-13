fa55681da2dcc87eaa90d8b8f8589902da26ee3f
suchithrkar <suchith.rkar@gmail.com>
2025-11-13 23:55:07 +0530

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,3 +1,4 @@
+// ================== Firebase Initialization (Shared) ==================
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 
 export const firebaseConfig = {

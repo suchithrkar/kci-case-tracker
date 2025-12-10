a2bee8e370043e21e43d93cdab68ffa2d5e226f0
suchithrkar <suchith.rkar@gmail.com>
2025-12-10 09:01:31 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -9,7 +9,7 @@
    - Audit Modal
    ========================================================= */
 
-/* =========================================================
+/* ========================================================
    SINGLE IMPORT BLOCK (Do NOT add any more imports)
    ======================================================== */
 import {
@@ -1926,6 +1926,7 @@ function subscribeStatsCases() {
   // (We only load on demand using loadStatsCasesOnce)
   return;
 }
+
 
 
 

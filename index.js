949fe31e01fb34585578f817ab7f83a4c211bbdd
suchithrkar <suchith.rkar@gmail.com>
2025-12-18 19:30:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* =======================================================================
    PHASE 1 — CORE ENGINE REBUILD
-   ======================================================================= */
+   ===================================================================== */
 
 import {
   auth,
@@ -2527,6 +2527,7 @@ negBtn.addEventListener("mouseenter", () => {
 negBtn.addEventListener("mouseleave", () => {
     globalTooltip.classList.remove("show-tooltip");
 });
+
 
 
 

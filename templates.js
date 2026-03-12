44ecfdf29e9419cf398fbc7ca15b3fb2240739bb
suchithrkar <suchith.rkar@gmail.com>
2026-03-12 12:57:24 +0530

    Update templates.js

--- templates.js
+++ templates.js
@@ -1,48 +1,15 @@
 export const MAIL_TEMPLATES = {
 
-  ncm1_onsite: {
-    subject: "",
-    body: ""
-  },
+  ncm1_onsite: { subject:"", body:"" },
+  ncm2_onsite: { subject:"", body:"" },
+  closure_onsite: { subject:"", body:"" },
 
-  ncm2_onsite: {
-    subject: "",
-    body: ""
-  },
+  ncm1_offsite: { subject:"", body:"" },
+  ncm2_offsite: { subject:"", body:"" },
+  closure_offsite: { subject:"", body:"" },
 
-  closure_onsite: {
-    subject: "",
-    body: ""
-  },
-
-  ncm1_offsite: {
-    subject: "",
-    body: ""
-  },
-
-  ncm2_offsite: {
-    subject: "",
-    body: ""
-  },
-
-  closure_offsite: {
-    subject: "",
-    body: ""
-  },
-
-  ncm1_parts: {
-    subject: "",
-    body: ""
-  },
-
-  ncm2_parts: {
-    subject: "",
-    body: ""
-  },
-
-  closure_parts: {
-    subject: "",
-    body: ""
-  }
+  ncm1_parts: { subject:"", body:"" },
+  ncm2_parts: { subject:"", body:"" },
+  closure_parts: { subject:"", body:"" }
 
 };

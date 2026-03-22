f962d675f1a0b5701c0862f55d80fa4d53acc5e8
suchithrkar <suchith.rkar@gmail.com>
2026-03-22 21:52:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4150,7 +4150,13 @@ function applyTemplateVariables(text, caseData, templateKey = "") {
 
 async function copyTemplateRich(text) {
 
-  const html = `<div style="font-family:Arial;font-size:15px;line-height:1.4;">${text.replace(/\n/g,"<br>")}</div>`;
+  const html = `
+    <div style="font-family:Arial;font-size:15px;line-height:1.5;">
+      ${text
+        .split("\n")
+        .map(line => `<div>${line || "&nbsp;"}</div>`)
+        .join("")}
+    </div>`;
 
   const blobHtml = new Blob([html], { type: "text/html" });
   const blobText = new Blob([text], { type: "text/plain" });
